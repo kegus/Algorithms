47efd58c224d8b1b4daf13e845a50f1911e5e18f
kegus <kegus@mail.ru>
2019-04-15 09:56:44 +0600

    Первый запуск.

--- src/Lesson5/Knapsack/Knapsack.java
+++ src/Lesson5/Knapsack/Knapsack.java
@@ -2,9 +2,11 @@ package Lesson5.Knapsack;
 
 public class Knapsack {
     public static void main(String[] args) {
-        Goods[] knapsack = {new Goods("it1", 2, 3),
-                            new Goods("it2", 5, 7),
-                            new Goods("it3", 7, 2),
+        Goods[] knapsack = {new Goods("чайник", 2, 3),
+                            new Goods("пылесос", 5, 7),
+                            new Goods("бочка", 7, 2),
+                            new Goods("ноутбук", 4, 8),
+                            new Goods("телефон", 1, 5),
                         };
         Knapsacks work = new Knapsacks(knapsack);
         System.out.println(work);
--- src/Lesson5/Knapsack/Knapsacks.java
+++ src/Lesson5/Knapsack/Knapsacks.java
@@ -23,6 +23,28 @@ public class Knapsacks {
         return res;
     }
 
+    public boolean isEmpty() {
+        for (int i = 0; i < knapsack.length; i++) {
+            if (!knapsack[i].isTaken()) return false;
+        }
+        return true;
+    }
+
+    public void process() {
+        int i = 0;
+        int n = 1;
+        while (!isEmpty()) {
+            knapsack[i].setTaken(true);
+            Goods[] tmp = new Goods[knapsack.length - n];
+            for (int j = 0, k = 0; j < knapsack.length; j++) {
+                if (!knapsack[j].isTaken()) {
+                    tmp[k] = knapsack[j];
+                    k++;
+                }
+            }
+        }
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
