23100132b09918e93eed7bb4a11d545fadb3738f
kegus <kegus@mail.ru>
2019-04-17 10:52:04 +0600

    Граф из 10 вершин. Случайным образом добавил рёбра. Нашёл кратчайший путь.

--- src/Lesson7/Program.java
+++ src/Lesson7/Program.java
@@ -1,8 +1,27 @@
 package Lesson7;
 
+import java.util.Random;
+
 public class Program {
     public static void main(String[] args) {
-        Graph graph = new Graph(6);
+        Random rnd = new Random();
+        int vertexCount = 10;
+        int edgeCount = 15;
+        int rndV1 = -1;
+        int rndV2 = -1;
+
+        Graph graph = new Graph(vertexCount);
+        for (int i = 0; i < edgeCount; i++) {
+            rndV1 = rnd.nextInt(vertexCount);
+            rndV2 = rnd.nextInt(vertexCount);
+            graph.addEdge(rndV1, rndV2);
+        }
+        BreadthFirstPaths bfs = new BreadthFirstPaths(graph, 0);
+        System.out.println(bfs.hasPathTo(vertexCount-2));
+        System.out.println(bfs.pathTo(vertexCount-2));
+        System.out.println(bfs.distTo(vertexCount-2));
+
+        /*Graph graph = new Graph(6);
         graph.addEdge(0, 2);
         graph.addEdge(0, 1);
         graph.addEdge(1, 2);
@@ -14,6 +33,6 @@ public class Program {
         BreadthFirstPaths bfs = new BreadthFirstPaths(graph, 0);
         System.out.println(bfs.hasPathTo(3));
         System.out.println(bfs.pathTo(3));
-        System.out.println(bfs.distTo(3));
+        System.out.println(bfs.distTo(3));*/
     }
 }
