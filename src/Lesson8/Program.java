67bb6e2f3aa0d5f81387918cbca4acae0594b192
kegus <kegus@mail.ru>
2019-04-21 00:04:01 +0600

    Начальная загрузка.

---
+++ src/Lesson8/Program.java
@@ -0,0 +1,15 @@
+package Lesson8;
+
+public class Program {
+    public static void main(String[] args) {
+        MyChainingHashMap<Character, Integer> hm = new MyChainingHashMap<>();
+        hm.put('a', 2);
+        hm.put('x', 4);
+        hm.put('r', 6);
+        hm.put('c', 12);
+        hm.put('d', 8);
+        hm.put('u', 9);
+        hm.put('t', 19);
+        hm.put('p', 1);
+    }
+}
